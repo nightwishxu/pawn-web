b11477e27c097c5c92acff8e4f0134bf2054196b
王必成 <3399425930@qq.com>
2021-05-20 16:35:03 +0800

    更新替换文件

--- static/config/index-qa.js
+++ static/config/index-qa.js
@@ -1,14 +1,14 @@
 /**
  * 测试环境
  */
-; (function () {
+;(function () {
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8090/pawn';
+  window.SITE_CONFIG['baseUrl'] = 'http://localhost:8080/pawn';
 
   // cdn地址 = 域名 + 版本号
-  window.SITE_CONFIG['domain'] = './'; // 域名
+  window.SITE_CONFIG['domain']  = './'; // 域名
   window.SITE_CONFIG['version'] = '';   // 版本号(年月日时分)
-  window.SITE_CONFIG['cdnUrl'] = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
+  window.SITE_CONFIG['cdnUrl']  = window.SITE_CONFIG.domain + window.SITE_CONFIG.version;
 })();
